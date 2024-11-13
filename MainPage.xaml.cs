6f27b3c3e08f6daca236c0b3b75b2b517b2ffb49
Alexis Sebastian <164224762+alexisetechas@users.noreply.github.com>
2024-11-13 15:25:05 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace PokeApi
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"CLicou {count} time";
+            else
+                CounterBtn.Text = $"Clicou {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
